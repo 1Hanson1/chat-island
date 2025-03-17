f079aab9250258a4f42f5fa63c82169753592dcb
1Hanson1 <115934088+1Hanson1@users.noreply.github.com>
2025-03-17 11:10:29 +0800

    1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,21 @@
 <template>
-    <header class="bg-blue-600 text-white p-4 flex justify-between items-center">
-      <div class="text-xl font-bold">LOGO</div>
-      <nav>
-        <ul class="flex space-x-4">
-          <li><a href="#" class="hover:underline">首页</a></li>
-          <li><a href="#" class="hover:underline">产品</a></li>
-          <li><a href="#" class="hover:underline">关于</a></li>
-          <li><a href="#" class="hover:underline">联系</a></li>
-        </ul>
-      </nav>
-    </header>
-  </template>
-  
-  <script>
-  export default {
-    name: "Header"
-  };
-  </script>
-  
-  <style scoped></style>
-  
+  <header class="bg-blue-600 text-white p-4 flex justify-between items-center">
+    <div class="text-xl font-bold">LOGO</div>
+    <nav>
+      <ul class="flex space-x-4">
+        <li><a href="#" class="hover:underline">首页</a></li>
+        <li><a href="#" class="hover:underline">产品</a></li>
+        <li><a href="#" class="hover:underline">关于</a></li>
+        <li><a href="#" class="hover:underline">联系</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped></style>
