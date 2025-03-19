338e459a046995978118339715c64c2637c2d1fe
1Hanson1 <115934088+1Hanson1@users.noreply.github.com>
2025-03-19 20:20:55 +0800

    1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="bg-blue-600 text-white p-4 flex justify-between items-center">
     <div class="text-xl font-bold">LOGO</div>
-    <nav class="flex-1">
-      <ul class="flex space-x-6">
+    <nav class="flex-shrink-0">
+      <ul class="flex gap-8">
         <li><a href="#" class="hover:underline">首页</a></li>
         <li><a href="#" class="hover:underline">余量查询</a></li>
         <li><a href="#" class="hover:underline">助手总览</a></li>
